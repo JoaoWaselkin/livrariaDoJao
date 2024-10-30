8384f9f358aa4766aa86902f4c75d345461a30aa
JoaoWaselkin <Joaorwl@gmail.com>
2024-10-29 21:00:56 -0300

    chore:atualizei a estrutura da criação do projeto da livraria

--- src/migrations/migration.ts
+++ src/migrations/migration.ts
@@ -1,22 +1,23 @@
+
 import pool from '../config/database';
 
 const createBooksTable = async () => {
-  const books = await pool.connect();
+  const client = await pool.connect();
   try {
     const queryText = `
       CREATE TABLE IF NOT EXISTS books (
         isbn13 SERIAL PRIMARY KEY,
         titlel VARCHAR(100) NOT NULL,
-        subtitle VARCHAR(100) UNIQUE NOT NULL,
+        subtitle VARCHAR(100) NOT NULL,
         price NUMERIC(10,2) NOT NULL
       );
     `;
-    await books.query(queryText);
+    await client.query(queryText);
     console.log('Tabela "books" criada com sucesso!');
   } catch (err) {
     console.error('Erro ao criar tabela:', err);
   } finally {
-    books.release();
+    client.release();
   }
 };
 
--- src/routes/booksRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getBooks, addBooks } from '../controllers/booksController';
-
-const router = Router();
-
-router.get('/books', getBooks);
-router.post('/books', addBooks);
-
-export default router;
