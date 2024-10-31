da1007fe18790c345d781ff04d46cedd195aebfc
JoaoWaselkin <Joaorwl@gmail.com>
2024-10-30 22:45:41 -0300

    feat:criação de novas tabelas para o banco de dados

--- src/migrations/migration.ts
+++ src/migrations/migration.ts
@@ -5,9 +5,9 @@ const createBooksTable = async () => {
   const client = await pool.connect();
   try {
     const queryText = `
-      CREATE TABLE IF NOT EXISTS books (
-        isbn13 SERIAL PRIMARY KEY,
-        titlel VARCHAR(100) NOT NULL,
+      CREATE TABLE IF NOT EXISTS books_on_the_table (
+        isbn SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
         subtitle VARCHAR(100) NOT NULL,
         price NUMERIC(10,2) NOT NULL
       );
